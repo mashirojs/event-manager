41cd18d0a86aac278c2f9e36dc97343927ec7d4f
mashirojs <mashiro.js@gmail.com>
2022-08-12 18:31:52 +0900

    add source

---
+++ EventManager.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+
+namespace Mashirojs.Event
+{
+    public interface IEventListenerBase {}
+    
+    public interface IEventListener<in T> : IEventListenerBase where T : struct {
+        void OnEvent(T e);
+    }
+
+    public static class EventManager {
+        private static Dictionary<Type, List<IEventListenerBase>> _listeners = new Dictionary<Type, List<IEventListenerBase>>();
+
+        public static void AddListener<T>(IEventListener<T> listener) where T: struct 
+        {
+            if (!_listeners.ContainsKey(typeof(T)))
+            {
+                _listeners.Add(typeof(T), new List<IEventListenerBase>());
+            }
+
+            if (!_listeners[typeof(T)].Contains(listener))
+            {
+                _listeners[typeof(T)].Add(listener);
+            }
+        }
+
+        public static void RemoveListener<T>(IEventListener<T> listener) where T: struct
+        {
+            _listeners[typeof(T)].Remove(listener);
+        }
+
+        public static void SendEvent<T>(T e) where T : struct
+        {
+            var listeners = _listeners[typeof(T)];
+            foreach (var listener in listeners)
+            {
+                (listener as IEventListener<T>).OnEvent(e);
+            }
+        }
+    }
+}
